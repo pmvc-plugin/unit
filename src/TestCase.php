f7ec37ecb5518fbf4a36796461628b92caf72d8a
Hill <hill@kimo.com>
2021-04-01 06:06:06 +0800

    try fixed unit test

--- src/TestCase.php
+++ src/TestCase.php
@@ -7,6 +7,9 @@ namespace {
         }
         class PHPUnit_Framework_Error extends Exception
         {
+            public function __construct($message = "" , $code = 0 , $previous = null, $line = null) {
+                parent::__construct($message, $code, $previous);
+            }
         }
     }
 }
