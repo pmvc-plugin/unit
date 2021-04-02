28b169f65719a6dff62e1b5475230e8c635528ab
Hill <hill@kimo.com>
2021-04-03 03:51:26 +0800

    Add throw support

---
+++ src/_throw.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PMVC;
+
+use Exception;
+
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__ . '\WrapThrow';
+
+class WrapThrow
+{
+    public function __invoke($willThrow, $caller, $phpunit, $error = true)
+    {
+        $annotation = \PMVC\plug('annotation');
+        $doc = $annotation->get($caller);
+        $expectedException = get($doc, 'expectedException');
+        $expectedExceptionMessage = get($doc, 'expectedExceptionMessage');
+        if (!empty($expectedException)) {
+            $phpunit->expectException($expectedException);
+        }
+        if (!empty($expectedExceptionMessage)) {
+            $phpunit->expectExceptionMessage($expectedExceptionMessage);
+        }
+        if (!$error) {
+            $willThrow();
+        } else {
+            try {
+                $willThrow();
+            } catch (Exception $e) {
+                throw new PMVCUnitException($e->getMessage(), 0);
+            }
+        }
+    }
+}
