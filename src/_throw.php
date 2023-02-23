29ea1eed3b0a3c244c65220a66e199083d76b6eb
Hill <hill@kimo.com>
2023-02-23 16:54:09 +0800

    Add convertErrorsToExceptions

--- src/_throw.php
+++ src/_throw.php
@@ -10,8 +10,19 @@ class WrapThrow
 {
     public $caller;
 
+    public function handleError(
+        $number,
+        $message,
+        $file = null,
+        $line = null,
+        $context = null
+    ) {
+        throw new Exception($message, $number);
+    }
+
     public function __invoke($willThrow, $caller, $phpunit, $error = true)
     {
+        set_error_handler([$this, 'handleError']);
         $annotation = \PMVC\plug('annotation');
         $doc = $annotation->get($caller, true);
         $expectedException = get($doc, 'expectedException');
